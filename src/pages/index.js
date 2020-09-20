30b2202174c5719f0707564d945b52806105fd57
Jeffer <jeffersonyajamin@gmail.com>
2020-09-20 19:30:58 +0200

    Añádiendo el Seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import useSeo from "../hooks/use-seo"
+import Helmet from "react-helmet"
 import ContenidoIndex from "../components/contenidoIndex"
 import ListProjects from "../components/listProjects"
 import useProyects from "../hooks/use-proyects"
@@ -8,10 +9,18 @@ import "@fortawesome/fontawesome-free/css/all.min.css"
 import Footer from "../components/footer"
 const IndexPage = () => {
   const proyects = useProyects()
+  const seo = useSeo()
+
+  const {
+    fallbackSeo: { description, title },
+  } = seo
   return (
     <>
       <Layout>
-        <SEO title="Home" />
+        <Helmet>
+          <title>{title} - Portfolio</title>
+          <meta name="description" content={description} />
+        </Helmet>
         <ContenidoIndex color="gray" />
         <div className="container mx-auto text-center w-full">
           <i className="fas fa-arrow-down animate-bounce  text-gray-500 text-2xl"></i>
@@ -20,7 +29,6 @@ const IndexPage = () => {
           <h1 className="font-bold  text-3xl uppercase text-gray-500 py-12 text-center">
             Portfolio
           </h1>
-
           {proyects.map(proyect => (
             <ListProjects key={proyect.id} proyect={proyect} />
           ))}
